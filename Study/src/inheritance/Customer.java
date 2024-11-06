92575de18f4d6dff67ccb12a838d4eb74e8e157b
DoHwa <84370817+D-o-H-w-a@users.noreply.github.com>
2024-11-06 17:40:01 +0900

    생성자 호출 print 추가
    Java Book 241 Page

--- Study/src/inheritance/Customer.java
+++ Study/src/inheritance/Customer.java
@@ -2,7 +2,7 @@ package inheritance;
 
 public class Customer {
 
-    // private
+    // 멤버 변수
 //	private int customerID; // 고객 아이디
 //	private String customerName; // 고객 이름
 //	private String customerGrade; // 고객 등급
@@ -40,16 +40,20 @@ public class Customer {
         this.customerGrade = customerGrade;
     }
 
+    // 디폴트 생성자
     public Customer() {
         customerGrade =  "SILVER"; // 기본 등급
         bonusRatio = 0.01; // 보너스 포인트 기본 적립 비율
+        System.out.println("Customer() 생성자 호출");
     }
 
+    // 보너스 포인트 적립, 지불 가격 계산 메서드
     public int calcPrice(int price) {
         bonusPoint += price * bonusRatio; // 보너스 포인트 계산
         return price;
     }
 
+    // 고객 정보를 반환하는 메서드
     public String showCustomerInfo() {
         return customerName + " 님의 등급은 " + customerGrade + " 이며, 보너스 포인트는 " + bonusPoint + " 입니다.";
     }
