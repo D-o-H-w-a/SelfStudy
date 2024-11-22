6a79a859045c0be1c59707e12d7c0925a5ccc646
DoHwa <84370817+D-o-H-w-a@users.noreply.github.com>
2024-11-22 09:56:49 +0900

    독학 공부용 자료 저장

--- Study/src/inheritance/CustomerTest1.java
+++ Study/src/inheritance/CustomerTest1.java
@@ -15,5 +15,23 @@ public class CustomerTest1 {
         customerKim.setCustomerName("김유신");
         customerKim.bonusPoint = 10000;
         System.out.println(customerKim.showCustomerInfo());
+
+        Animal lion = new Animal();
+        lion.id = 1;
+        lion.name = "사자";
+        lion.age = 5;
+        lion.type = "콩고";
+        lion.birth = "2019-01-13";
+        System.out.println("현재 동물의 번호는 " + lion.id + "번 이며 동물의 이름은 " + lion.name + "이고"
+        + " 나이는 " + lion.age + "살이며 동물의 종은 " + lion.type + "입니다. 해당 동물의 출생일은 " + lion.birth + " 입니다.");
+
+        Animal tiger = new Animal();
+        tiger.id = 2;
+        tiger.name = "호랑이";
+        tiger.age = 7;
+        tiger.type = "벵골";
+        tiger.birth = "2017-04-22";
+        System.out.println("현재 동물의 번호는 " + tiger.id + "번 이며 동물의 이름은 " + tiger.name + "이고"
+                + " 나이는 " + tiger.age + "살이며 동물의 종은 " + tiger.type + "입니다. 해당 동물의 출생일은 " + tiger.birth + " 입니다.");
     }
 }
