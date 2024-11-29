9de7865be53d7b8f508ea3bb90d6a4a80c6f6675
DoHwa <84370817+D-o-H-w-a@users.noreply.github.com>
2024-11-29 10:02:01 +0900

    싱글톤 독학 저장

--- Study/src/singletonStudy/Animal.java
+++ Study/src/singletonStudy/Animal.java
@@ -3,6 +3,10 @@ package singletonStudy;
 public class Animal {
 
     public static void main(String[] args) {
+        Zoo zoo =  Animals.instance().getZoo();
 
+        zoo.setName("기장");
+
+        zoo.prn();
     }
 }
