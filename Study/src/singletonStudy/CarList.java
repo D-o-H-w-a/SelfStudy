c3960a1b6f7db5e91dab5c7107bc81d9561a25e8
DoHwa <wollyang583@naver.com>
2024-11-29 14:42:37 +0900

    저장

--- Study/src/singletonStudy/CarList.java
+++ Study/src/singletonStudy/CarList.java
@@ -1,6 +1,23 @@
 package singletonStudy;
 
+import java.awt.*;
+import java.util.Objects;
+
 public class CarList {
 
+    private List company = new List();
 
+    public List getCompany() {
+
+        if (Objects.isNull(company)) {
+            company = new List();
+            return company;
+        }
+
+        return company;
+    }
+
+    public void setCompany(List company) {
+        this.company = company;
+    }
 }
--- Study/src/singletonStudy/SingletonManager.java
+++ Study/src/singletonStudy/SingletonManager.java
@@ -4,12 +4,23 @@ public class SingletonManager {
 
     private static SingletonManager singManager;
 
-    private CarList zoo;
+    private CarList carList;
+
     public static SingletonManager instance() {
         if (singManager == null) {
             singManager = new SingletonManager();
         }
 
         return singManager;
+    }
+
+    public CarList getCarList() {
+        if (carList == null) {
+            carList = new CarList();
+        }
+        return carList;
+    }
+    public void setCarList(CarList carList) {
+        this.carList = carList;
     }
 }
