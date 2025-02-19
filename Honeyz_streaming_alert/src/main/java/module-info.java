7ac120f9e80c5809af7c7ad9012a72abf6b87761
DoHwa <wollyang583@naver.com>
2025-02-20 07:35:32 +0900

    11버전으로 재변경

--- Honeyz_streaming_alert/src/main/java/module-info.java
+++ Honeyz_streaming_alert/src/main/java/module-info.java
@@ -5,7 +5,6 @@ module org.example.honeyz_streaming_alert {
     requires java.desktop;
     requires java.prefs;
 
-
     opens org.example.honeyz_streaming_alert to javafx.fxml;
     exports org.example.honeyz_streaming_alert;
 }
